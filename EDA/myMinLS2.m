36ad0d56630453979f0feb0fa95707ebb4827d6b
Matti Morzfeld <mmo@math.arizona.edu>
2017-11-20 11:40:09 -0700

    Add files via upload

---
+++ EDA/myMinLS2.m
@@ -0,0 +1,12 @@
+function [x,resnorm,residual,exitflag,output,lambda,jacobian]=myMinLS2(xo,z,Gap,dt,F,H,R,mu,Lb)
+func=@(x)funcF2(x,z,Gap,dt,F,H,R,mu,Lb);
+options = optimoptions('lsqnonlin','Algorithm','levenberg-marquardt',...
+    'SpecifyObjectiveGradient',true,...
+    'Diagnostics','off',...
+    'Display','off');%'iter-detailed');
+n = size(H,2);
+ub = inf*ones(n,1);
+lb = -ub;
+[x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(func,xo,lb,ub,options);
+
+
